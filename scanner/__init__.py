5bbbdd38dd2024149558f050ea767401ab7fed19
Tharanika <tharanikabalasubramanian@gmail.com>
2026-09-18 22:31:05 +0530

    Remove version and exports from __init__.py
    Removed versioning and exported module list from __init__.py

--- scanner/__init__.py
+++ scanner/__init__.py
@@ -1,11 +1,1 @@
-__version__ = "1.0.0"
 
-__all__ = [
-    "model_loader",
-    "model_validator",
-    "weight_analyzer",
-    "anomaly_detector",
-    "backdoor_detector",
-    "integrity_checker",
-    "risk_engine",
-]
